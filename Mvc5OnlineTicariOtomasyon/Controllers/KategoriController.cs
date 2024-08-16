07533f78356893f40789f03b5e63b5e5ad11dfbe
ArdaCimen <cimenarda.yaz@gmail.com>
2024-08-16 14:12:49 +0300

    Temanın Projeye Dahil Edilmesi,Kategori CRUD İşlemleri

---
+++ Mvc5OnlineTicariOtomasyon/Controllers/KategoriController.cs
@@ -0,0 +1,68 @@
+﻿using Mvc5OnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5OnlineTicariOtomasyon.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        Context db = new Context();
+        public ActionResult Index()
+        {
+            var kategoriler = db.Kategoris.ToList();
+            
+            return View(kategoriler);
+        }
+
+
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KategoriEkle(Kategori k)
+        {
+            db.Kategoris.Add(k);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult KategoriSil(int id)
+        {
+            var kategoriyibul = db.Kategoris.Find(id);
+            db.Kategoris.Remove(kategoriyibul);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public ActionResult KategoriGuncelle(int id)
+        {
+            var kategoriyibul = db.Kategoris.Find(id);
+
+            return View("KategoriGuncelle",kategoriyibul);
+        }
+
+        [HttpPost]
+        public ActionResult KategoriGuncelle(Kategori k)
+        {
+            var kategoriyibul = db.Kategoris.Find(k.KategoriID);
+
+            kategoriyibul.KategoriAd = k.KategoriAd;
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
+
+    }
+}

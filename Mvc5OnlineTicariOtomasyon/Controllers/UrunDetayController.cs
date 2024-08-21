dc9e2aa1166825e6d64752a34f5e197e25b96060
ArdaCimen <cimenarda.yaz@gmail.com>
2024-08-21 12:11:40 +0300

    Ürün Detay Sayfası-Tema İşlemleri

---
+++ Mvc5OnlineTicariOtomasyon/Controllers/UrunDetayController.cs
@@ -0,0 +1,25 @@
+﻿using Mvc5OnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5OnlineTicariOtomasyon.Controllers
+{
+    public class UrunDetayController : Controller
+    {
+        // GET: UrunDetay
+        Context db = new Context();
+
+        public ActionResult Index()
+        {
+            Class1 cs =new Class1();
+
+            cs.Deger1 =db.Uruns.Where(x=>x.UrunID==1).ToList();
+            cs.Deger2 =db.Detays.Where(x=>x.DetayID==1).ToList();
+
+            return View(cs);
+        }
+    }
+}
--- Mvc5OnlineTicariOtomasyon/Models/Siniflar/Context.cs
+++ Mvc5OnlineTicariOtomasyon/Models/Siniflar/Context.cs
@@ -18,6 +18,7 @@ namespace Mvc5OnlineTicariOtomasyon.Models.Siniflar
         public DbSet<Personel> Personels { get; set; }
         public DbSet<SatisHareket> SatisHarekets { get; set; }
         public DbSet<Urun> Uruns { get; set; }
+        public DbSet<Detay> Detays { get; set; }
 
 
     }
